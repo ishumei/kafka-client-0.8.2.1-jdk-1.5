e6b1b73475f10011e33c961a794b1c5749ccde08
Liang Kun <liangkun@outlook.com>
2015-09-07 22:07:04 +0800

    fix test

--- src/main/java/org/apache/kafka/common/record/CompressionType.java
+++ src/main/java/org/apache/kafka/common/record/CompressionType.java
@@ -20,7 +20,7 @@ package org.apache.kafka.common.record;
  * The compression type to use
  */
 public enum CompressionType {
-    NONE(0, "none", 1.0f), GZIP(1, "gzip", 0.5f), SNAPPY(2, "snappy", 0.5f), LZ4(3, "lz4", 0.5f);
+    NONE(0, "none", 1.0f), GZIP(1, "gzip", 0.5f), SNAPPY(2, "snappy", 0.5f)/*, LZ4(3, "lz4", 0.5f)*/;
 
     public final int id;
     public final String name;
@@ -40,8 +40,8 @@ public enum CompressionType {
                 return GZIP;
             case 2:
                 return SNAPPY;
-            case 3:
-                return LZ4;
+//            case 3:
+//                return LZ4;
             default:
                 throw new IllegalArgumentException("Unknown compression type id: " + id);
         }
@@ -54,8 +54,8 @@ public enum CompressionType {
             return GZIP;
         else if (SNAPPY.name.equals(name))
             return SNAPPY;
-        else if (LZ4.name.equals(name))
-            return LZ4;
+//        else if (LZ4.name.equals(name))
+//            return LZ4;
         else
             throw new IllegalArgumentException("Unknown compression name: " + name);
     }
